b6b2468d28f5e93903ce5484aa7edc05e8975e10
nboierzyc <49646349+nboierzyc@users.noreply.github.com>
2023-10-15 16:30:49 -0500

    mp3.1

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -4,8 +4,8 @@
 
 #include "types.h"
 #define KEYBOARD_PORT 0x60
-#define KEYBOARD_IRQ 1
-#define SCANCODE_SIZE 99
+#define KEYBOARD_IRQ 1 /*keyboard's irq number*/
+#define SCANCODE_SIZE 99 /*0x59 is the largest scancode for pressing*/
 extern void key_init(void);
 
 #endif
