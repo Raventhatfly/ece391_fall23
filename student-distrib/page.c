19cb9b6d0dbbb393d3b5f3d65ddf4d906e3d4257
Astarojth <108380976+Astarojth@users.noreply.github.com>
2023-10-15 14:48:58 -0500

    page-version2

--- student-distrib/page.c
+++ student-distrib/page.c
@@ -3,13 +3,13 @@
 #include "types.h"
 
 /*
-    *  void page_initialize()
+    *  void page_init()
     *    DESCRIPTION: initialize the page
     *    INPUT: none
     *    OUTPUT: none
     *    SIDE EFFECT: set the page directory and page table
 */
-void page_initialize() {
+void page_init() {
     int i=0;
     for (i=0; i<ENTRIES; i++) {     /* initialize all entries of table and dict to 0 */
         page_directory_entries[i] = INITIAL_STATE;
@@ -19,7 +19,7 @@ void page_initialize() {
     int video_idx = VIDEO >> 12;    /* get the index of video memory, move 12 bit to the right for reduce 0 */
     page_table_entries[video_idx] = VIDEO | THREE_BITMASK; /* set the video memory,7 is used as the mask of last 3 bit*/
     page_directory_entries[0] = ((uint32_t)page_table_entries)| THREE_BITMASK; /* I don't really know */
-    page_directory_entries[1] = KERNEL | THREE_BITMASK; /* set the kernel memory */
+    page_directory_entries[1] = KERNEL | MB_BITMASK; /* set the kernel memory */
     /* use asm to set the control register bits to their proper values */
     /* so tells the hardware where the page directory is located and enables hardware support for paging */
     asm volatile("              \n\
