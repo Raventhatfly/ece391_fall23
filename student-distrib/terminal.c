d1f2434cb16f7935b02bfb95de513cc9bab35ad7
Astarojth <108380976+Astarojth@users.noreply.github.com>
2023-10-22 14:52:21 -0500

    terminal_ver5.3

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -18,7 +18,7 @@ termin_t my_terminal;
     *   SIDE EFFECTS: clear the buffer
 */
 uint32_t buffer_clear(){
-    for (i = 0; i < 128; i++) my_terminal.terminal_buffer[i] = ' '; /*clear the buffer of all the 128 chars*/
+    for (i = 0; i < BUFFER_SIZE; i++) my_terminal.terminal_buffer[i] = ' '; /*clear the buffer of all the 128 chars*/
     my_terminal.buffer_iterator = 0;
     return 0;
 }
@@ -33,12 +33,12 @@ uint32_t buffer_clear(){
 */
 void screen_cpy(){
     screen_x = 0;          /*set the position of the row*/
-    if (screen_y < 24){    /*if the row is not the last row(there are 25 rows so 24), go to the next row*/
+    if (screen_y < COLS-1){    /*if the row is not the last row(there are 25 rows so 24), go to the next row*/
         screen_y++;         
     }else{
         memcpy(video_mem, video_mem+COLS*2, COLS*(ROWS-1)*2); /* or copy the screen to the next line*/
         for (i = 0; i < COLS; i++) *(uint32_t *)(video_mem + (COLS*(ROWS-1)+i)*2 ) = ' ';  /*clear the screen with blank*/
-        for (i = 0; i < COLS; i++) *(uint32_t *)(video_mem + (COLS*(ROWS-1)+i)*2 + 1) = 0x7;  /*set the ATTRIB of the screen, use 0x7 as it can't be directly link to lib.c*/
+        for (i = 0; i < COLS; i++) *(uint32_t *)(video_mem + (COLS*(ROWS-1)+i)*2 + 1) = ATTRIB;  /*set the ATTRIB of the screen*/
     }
 }
 
@@ -51,7 +51,7 @@ void screen_cpy(){
     *   SIDE EFFECTS: read the input from keyboard
 */
 uint32_t terminal_read(unsigned char input){
-    if (input == 0xA){             /* if the input is enter, clear the buffer and copy the screen as moving to next line*/
+    if (input == ENTER_ASC2){             /* if the input is enter, clear the buffer and copy the screen as moving to next line*/
         my_terminal.buffer_iterator=buffer_clear();  
         screen_cpy();
         my_terminal.cursor_x_coord=screen_x;
@@ -63,7 +63,7 @@ uint32_t terminal_read(unsigned char input){
         return -1;
     }
     if (screen_x==79){      /*if the input is at the end(the 80th char) of the line, set the buffer as enter to change to next line*/
-        my_terminal.terminal_buffer[my_terminal.buffer_iterator] = 0xA; /*if the input is at the end of the line, set the buffer as enter to change to next line*/
+        my_terminal.terminal_buffer[my_terminal.buffer_iterator] = ENTER_ASC2; /*if the input is at the end of the line, set the buffer as enter to change to next line*/
         my_terminal.buffer_iterator++;
         my_terminal.terminal_buffer[my_terminal.buffer_iterator] = input; /*set the buffer as the input to display*/
         screen_cpy();                                               /*copy the screen as moving to next line*/
@@ -87,10 +87,10 @@ void terminal_write(){
     printf("%c", my_terminal.terminal_buffer[my_terminal.buffer_iterator]); /*print the current char*/
     my_terminal.cursor_x_coord=screen_x;
     my_terminal.cursor_y_coord=screen_y;
-    *(uint32_t *)(video_mem + ((COLS * screen_y + screen_x) * 2) + 1) = 0x7; /*set the ATTRIB of the screen, use 0x7 as it can't be directly link to lib.c*/
+    *(uint32_t *)(video_mem + ((COLS * screen_y + screen_x) * 2) + 1) = ATTRIB; /*set the ATTRIB of the screen*/
     draw_cursor(my_terminal.cursor_x_coord, my_terminal.cursor_y_coord); /*redraw the cursor*/
     my_terminal.buffer_iterator++;
-    my_terminal.buffer_iterator= my_terminal.buffer_iterator % 128; /*if the buffer is full, set the iterator to 0*/
+    my_terminal.buffer_iterator= my_terminal.buffer_iterator % BUFFER_SIZE; /*if the buffer is full, set the iterator to 0*/
 }
 
 /* The following cursor functions are developing from https://wiki.osdev.org/Text_Mode_Cursor */
@@ -151,7 +151,7 @@ uint32_t terminal_close(){
 */
 uint32_t terminal_clear(){
     for (i = 0; i < ROWS * COLS; i++) *(uint32_t *)(video_mem + i*2) = ' ';
-    for (i = 0; i < ROWS * COLS; i++) *(uint32_t *)(video_mem + i*2 + 1) = 0x7; /*set the ATTRIB of the screen, use 0x7 as it can't be directly link to lib.c*/
+    for (i = 0; i < ROWS * COLS; i++) *(uint32_t *)(video_mem + i*2 + 1) = ATTRIB; /*set the ATTRIB of the screen*/
     i = 0;   
     screen_x = i;
     screen_y = i;
@@ -185,7 +185,7 @@ void terminal_init(){
 */
 uint32_t terminal_display(unsigned char input){
     int flag;
-    if (my_terminal.buffer_iterator >= 128) return -1; /*if the buffer is full, return -1 to inform the failure*/
+    if (my_terminal.buffer_iterator >= BUFFER_SIZE) return -1; /*if the buffer is full, return -1 to inform the failure*/
     flag = terminal_read(input);                    /*read the input, flag shows wthether succeed*/
     if (flag == -1) return flag;                    /*if the input is enter, return -1 to inform the failure*/
     terminal_write();                               /*if the input is not enter, write the input to screen*/
@@ -214,13 +214,13 @@ uint32_t terminal_delete(){
         screen_x--;             /*or just delete the last char of the current line*/
     }
     *(uint32_t *)(video_mem + ((COLS * screen_y + screen_x) * 2)) = ' '; /*initialize the char as blank and ATTRIB*/
-    *(uint32_t *)(video_mem + ((COLS * screen_y + screen_x) * 2) + 1) = 0x7; 
+    *(uint32_t *)(video_mem + ((COLS * screen_y + screen_x) * 2) + 1) = ATTRIB;
     my_terminal.cursor_x_coord=screen_x;
     my_terminal.cursor_y_coord=screen_y;
     draw_cursor(my_terminal.cursor_x_coord, my_terminal.cursor_y_coord); /*redraw the cursor*/
     my_terminal.terminal_buffer[my_terminal.buffer_iterator] = ' '; 
     my_terminal.buffer_iterator--;
-    my_terminal.buffer_iterator= my_terminal.buffer_iterator % 128; /*if the buffer is full, set the iterator to 0*/
+    my_terminal.buffer_iterator= my_terminal.buffer_iterator % BUFFER_SIZE; /*if the buffer is full, set the iterator to 0*/
     return 0;
 }
 
