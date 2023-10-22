5a8f20419ebd1612a9fd512e93e0fc20f8133103
nboierzyc <49646349+nboierzyc@users.noreply.github.com>
2023-10-21 22:22:57 -0500

    mp3.2

--- student-distrib/filesystem.c
+++ student-distrib/filesystem.c
@@ -95,6 +95,7 @@ int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length
     }
     if (index_node->length<offset+length)
         length=index_node->length=offset;
+    printf("%d\n",index_node->length);
     length_remain = length;
     data_block_t* data_block_ptr = (data_block_t*)(((inode_t*)((boot_block_t*)file_base+1))+inode_cnt);
     while(length_remain >= BLOCK_SIZE){
