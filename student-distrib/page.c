563c8b757cb66c0b416dc6378e7f5d4c945b1f27
nboierzyc <49646349+nboierzyc@users.noreply.github.com>
2023-11-08 09:57:26 -0600

    mp3.4

--- student-distrib/page.c
+++ student-distrib/page.c
@@ -14,15 +14,25 @@ void page_init() {
     for (i=0; i<ENTRIES; i++) {     /* initialize all entries of table and dict to 0 */
         page_directory_entries[i] = 0;
         page_table_entries[i] = 0;
+        video_table_entries[i] = 0;
     }
     int video_idx = VIDEO >> 12;    /* get the index of video memory, move 12 bit to the right for reduce 0 */
     page_table_entries[video_idx] = (VIDEO & 0xfffff000) | PRESENT_MASK; /* set the video memory*/
     page_table_entries[video_idx] |= R_AND_W_MASK;
+
     // page_table_entries[video_idx] |= USER_MASK;
     page_directory_entries[0] = ((uint32_t)page_table_entries & 0xfffff000)| PRESENT_MASK; /* set the page directory entries */
     page_directory_entries[0] |= R_AND_W_MASK; 
     // page_directory_entries[0] |= USER_MASK; 
+
     page_directory_entries[1] = KERNEL | MB_BITMASK; /* set the kernel memory */
+
+    uint32_t pg_dir_index = (USER_START_PLACE+FOUR_MBYTE) >> ADDR_SEARCH; /* get the index of page directory */
+    page_directory_entries[pg_dir_index] = 0; /* initialize the page directory entries */
+    page_directory_entries[pg_dir_index] |= PRESENT_MASK;
+    page_directory_entries[pg_dir_index] |= R_AND_W_MASK;
+    page_directory_entries[pg_dir_index] |= USER_MASK;
+    page_directory_entries[pg_dir_index] |= ((uint32_t)video_table_entries & 0xfffff000);
     /* use asm to set the control register bits to their proper values */
     /* so tells the hardware where the page directory is located and enables hardware support for paging */
     asm volatile("              \n\
@@ -66,18 +76,10 @@ void program_page_init(uint32_t program_id){
 }
 void set_map(uint8_t** screen_start)
 {
-    uint32_t pg_dir_index = (USER_START_PLACE+FOUR_MBYTE) >> ADDR_SEARCH; /* get the index of page directory */
-    page_directory_entries[pg_dir_index] = 0; /* initialize the page directory entries */
-    page_directory_entries[pg_dir_index] |= PRESENT_MASK;
-    page_directory_entries[pg_dir_index] |= R_AND_W_MASK;
-    page_directory_entries[pg_dir_index] |= USER_MASK;
-    page_directory_entries[pg_dir_index] |= ((uint32_t)video_table_entries & 0xfffff000);
-    uint32_t page_dir_addr = (uint32_t) &page_directory_entries;
-    asm volatile("     \n\
-    movl %0, %%eax     \n\
-    movl %%eax, %%cr3  \n\
-    "    
-    :
-    : "r" (page_dir_addr)
-    : "%eax");
+    uint32_t pg_dir_index = (VIDEO) >> 12; /* get the index of page directory */
+    video_table_entries[pg_dir_index] = 0; /* initialize the page directory entries */
+    video_table_entries[pg_dir_index] |= PRESENT_MASK;
+    video_table_entries[pg_dir_index] |= R_AND_W_MASK;
+    video_table_entries[pg_dir_index] |= USER_MASK;
+    video_table_entries[pg_dir_index] |= (VIDEO & 0xfffff000);
 }
