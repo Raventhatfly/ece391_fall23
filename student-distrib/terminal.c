3ba15e91a80104dbe8beaa50097488eb37d5553d
Raventhatfly <feiyangw.21@intl.zju.edu.cn>
2023-11-18 20:55:25 -0600

    update

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -189,7 +189,7 @@ int32_t terminal_read(int32_t fd, void* buf, int32_t nbytes){
     buffer_clear(curr_exe_terminal); 
     my_terminal[curr_exe_terminal].read_flag = 0; 
     while (!my_terminal[curr_exe_terminal].read_flag);  /*wait until the enter is pressed*/
-    my_terminal[curr_exe_terminal].terminal_buffer[my_terminal[curr_exe_terminal].buffer_iterator] = '\0';
+    my_terminal[curr_exe_terminal].terminal_buffer[my_terminal[curr_exe_terminal].buffer_iterator-1] = '\0';
     if (nbytes > BUFFER_SIZE) {     /*if the nbytes is larger than the buffer size, set the j as the buffer size*/
         j = BUFFER_SIZE;
     }else{
