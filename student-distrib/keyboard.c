3787fd0331e5b13bd5d817c930b41be3309278d4
nboierzyc <49646349+nboierzyc@users.noreply.github.com>
2023-11-30 22:45:28 -0600

    extra

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -95,7 +95,6 @@ void irq1_handler(void)
     cli();
     /*read from keyboard*/
     uint8_t key = inb(KEYBOARD_PORT);
-    printf("key: %x\n", key);
     /*
     change the special key to the flag
     */
